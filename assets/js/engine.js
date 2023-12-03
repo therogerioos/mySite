8b00c4e3a52dee88f5018524b7faedcc48505e53
therogerioos <ros.fsa@gmail.com>
2023-12-02 22:51:13 -0300

    add files update

--- assets/js/engine.js
+++ assets/js/engine.js
@@ -123,6 +123,90 @@ function goToPreviousPagett(){
 //alert("Altura do viewport: " + altura);
 
 
+function myFunction() {
+  var x = document.getElementById("myLinks");
+  if (x.style.display === "block") {
+    x.style.display = "none";
+  } else {
+    x.style.display = "block";
+  }
+}
+
+document.addEventListener("click", function(event) {
+  var icone = event.target;
+  
+  // Verifica se o clique ocorreu no elemento com a classe "seta-left"
+  if (icone.classList.contains("seta-left")) {
+    var elementoComID = encontrarElementoComID(icone);
+    
+    if (elementoComID) {
+      var idDoElemento = elementoComID.id;
+      var valorParcial = idDoElemento.split('-')[1];
+      var valorInteiro = parseInt(valorParcial);
+      if(valorInteiro == 1){
+        valorInteiro = 4;
+      } else {
+        valorInteiro = valorInteiro - 1;
+      };
+    } else {
+      console.log("Nenhum ID encontrado.");
+    }
+    var elementoAtual = document.getElementById(idDoElemento);
+    elementoAtual.classList.remove("carousel-item");
+    elementoAtual.classList.add("ocultando-carousel");
+    var novoID = "cr-" + valorInteiro;
+    var elementoNovo = document.getElementById(novoID);
+    elementoNovo.classList.remove("ocultando-carousel");
+    elementoNovo.classList.add("carousel-item");
+  }
+});
+
+
+document.addEventListener("click", function(event) {
+  var icone = event.target;
+  
+  // Verifica se o clique ocorreu no elemento com a classe "seta-right"
+  if (icone.classList.contains("seta-right")) {
+    var elementoComID = encontrarElementoComID(icone);
+    
+    if (elementoComID) {
+      var idDoElemento = elementoComID.id;
+      var valorParcial = idDoElemento.split('-')[1];
+      var valorInteiro = parseInt(valorParcial);
+      if(valorInteiro == 4){
+        valorInteiro = 1;
+      } else {
+        valorInteiro = valorInteiro + 1;
+      };
+    } else {
+      console.log("Nenhum ID encontrado.");
+    }
+    var elementoAtual = document.getElementById(idDoElemento);
+    elementoAtual.classList.remove("carousel-item");
+    elementoAtual.classList.add("ocultando-carousel");
+    var novoID = "cr-" + valorInteiro;
+    var elementoNovo = document.getElementById(novoID);
+    elementoNovo.classList.remove("ocultando-carousel");
+    elementoNovo.classList.add("carousel-item");
+  }
+});
+
+
+function encontrarElementoComID(elemento) {
+  var ancestral = elemento.parentElement;
+
+  // Iterar pelos ancestrais até encontrar um com um ID
+  while (ancestral) {
+    if (ancestral.id) {
+      return ancestral;
+    }
+    ancestral = ancestral.parentElement;
+  }
+  // Se nenhum ancestral com ID for encontrado, retornar null
+  return null;
+}
+
+
 const hamburger = document.getElementById("hamburger");
 const navLinks = document.getElementById("nav-links");
 
